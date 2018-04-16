05c255fd112d0a2e47380afc065d07777d3425fc
wimpieboy <wimflorijn@hotmail.com>
2018-04-17 00:57:09 +0200

    resultgetter improved -> write to csv file

--- resultsgetter.py
+++ resultsgetter.py
@@ -1,45 +1,59 @@
+import csv
 import subprocess
 import xml.etree.ElementTree
-import os
 import re
 
 generator_location = "C://Users//Wim//Downloads//groove-5_7_2-bin//groove-5_7_2//bin//generator.jar"
 gps_location = "C://Users//Wim//Documents//Universiteit//Year5Module3//SoftwareScience//GraphTransformProject//pentago_wim.gps"
 output_file = "C://Users//Wim//Documents//Universiteit//Year5Module3//SoftwareScience//GraphTransformProject//output.txt"
+result_file = "C://Users//Wim//Documents//Universiteit//Year5Module3//SoftwareScience//GraphTransformProject//result.csv"
 
-execution_amount = 25
-w,d  = 0, 0
-p1, p2 = 0, 0
-winners = []
+execution_amount = 50
 p = True
 
-for i in range(execution_amount):
-    subprocess.check_call("java -jar " + generator_location + " " + gps_location + " -f " + output_file, stdin=None, stdout=open(os.devnull, 'w'), stderr=None, shell=False)
-    r = xml.etree.ElementTree.parse(output_file).getroot()
-    result = r.findall(".//*[@from='n36'][@to='n45']")[0][0][0].text
+def write_result(result):
+    header = ["States", "Transitions", "Result", "Winner"]
+    with open(result_file, 'w', newline='') as csv_file:
+        w = csv.DictWriter(csv_file, header)
+        w.writeheader()
+        for row in result:
+            w.writerow(result[row])
 
-    if result == 'winner':
-        winner = r.findall(".//*[@from='n45'][@to='n45']")[1][0][0].text
-        name = re.search(r'let:name = string:"(\S+)"', winner).group(1)
-        if name == 'P1':
-            p1 += 1
-        elif name == 'P2':
-            p2 += 1
+def explore():
+    result = {}
+
+    for i in range(execution_amount):
+        single_result = {}
+
+        cmd_output = subprocess.check_output("java -jar " + generator_location + " " + gps_location + " -f " + output_file,
+                                        stdin=None, stderr=None, shell=False)
+        splitted_cmd_output = cmd_output.split()
+        for index, item in enumerate(splitted_cmd_output):
+            if item.decode("utf-8") == 'States:':
+                single_result['States'] = splitted_cmd_output[index + 1].decode("utf-8")
+        for index, item in enumerate(splitted_cmd_output):
+            if item.decode("utf-8") == 'Transitions:':
+                single_result['Transitions'] = splitted_cmd_output[index + 1].decode("utf-8")
+
+        root = xml.etree.ElementTree.parse(output_file).getroot()
+        game_result = root.findall(".//*[@from='n36'][@to='n45']")[0][0][0].text
+
+        if game_result == 'winner':
+            single_result['Result'] = 'Winner'
+            winner = root.findall(".//*[@from='n45'][@to='n45']")[1][0][0].text
+            name = re.search(r'let:name = string:"(\S+)"', winner).group(1)
+            single_result['Winner'] = name
         else:
-            print("ERROR player name mismatch: " + name)
-        w += 1
-    elif result == 'draw':
-        d += 1
-    else:
-        print("ERROR result mismatch: " + result)
-    if p:
-        print("Ctr: " + str(i+1) + ' / ' + str(execution_amount))
+            single_result['Result'] = 'Draw'
 
-print("Total number of runs: " + str(execution_amount))
-print("Draw amount: " + str(d))
-print("Win amount: "+str(w))
-print("Player P1 wins: " + str(p1))
-print("Player P2 wins: " + str(p2))
+        result[i] = single_result
+
+        if p:
+            print("Progress: " + str(i+1) + ' / ' + str(execution_amount))
+    return result
+
+write_result(explore())
+
 
 
 
